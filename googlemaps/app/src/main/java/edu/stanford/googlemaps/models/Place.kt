ad3fe4dbce0e08da519a712c60961a9ae410206f
Lily Liu <liu.xuanchi@hotmail.com>
2020-05-06 16:05:31 -0700

    initial commit

---
+++ googlemaps/app/src/main/java/edu/stanford/googlemaps/models/Place.kt
@@ -0,0 +1,6 @@
+package edu.stanford.googlemaps.models
+
+import java.io.Serializable
+
+data class Place(val title: String, val description: String,
+                 val latitude: Double, val longitude: Double) : Serializable
